5ddfafef482caa279766f23c3bee4cb6616ad6af
AnilEnsari <anilensari@gmail.com>
2023-07-29 17:40:44 +0300

    last2step

--- src/bileşenler/tablar.js
+++ src/bileşenler/tablar.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const Tablar = (konu) => {
   // GÖREV 3
   // ---------------------
@@ -13,16 +15,42 @@ const Tablar = (konu) => {
   //   <div class="tab">teknoloji</div>
   // </div>
   //
-}
+  const div11 = document.createElement("div");
+  div11.classList.add("topics");
+  for (let i = 0; i < konu.length; i++) {
+    let div12 = document.createElement("div");
+    div12.classList.add("tab");
+    div12.textContent = konu[i];
+    div11.append(div12);
+  }
+  return div11;
+};
 
 const tabEkleyici = (secici) => {
-  // GÖREV 4
-  // ---------------------
-  // Tek argümanı olarak bir css seçici alan bu işlevi uygulayın.
-  // Konuları bu uç noktadan almalıdır: `http://localhost:5001/api/konular` (console.log ile test edin!).
-  // Yanıtın içindeki konu dizisini bulun ve Tablar bileşenini kullanarak tabları oluşturun.
-  // Tabları, fonksiyona iletilen seçiciyle eşleşen DOM'daki öğeye ekleyin.
-  //
-}
+  const myTarget11 = document.querySelector(secici);
+  async function tabCalistir() {
+    await axios.get("http://localhost:5001/api/konular").then((response) => {
+      myTarget11.append(Tablar(response.data["konular"]));
+    });
+  }
+  return tabCalistir();
+};
 
-export { Tablar, tabEkleyici }
+// async function dataAl() {
+//   await ipAdresimiAl();
+//   axios
+//     .get(`https://apis.ergineer.com/ipgeoapi/${benimIP}`)
+//     .then((response) => {
+//       cardsHtml.append(ipControl(response.data));
+//     });
+// }
+// dataAl();
+
+// GÖREV 4
+// ---------------------
+// Tek argümanı olarak bir css seçici alan bu işlevi uygulayın.
+// Konuları bu uç noktadan almalıdır: `http://localhost:5001/api/konular` (console.log ile test edin!).
+// Yanıtın içindeki konu dizisini bulun ve Tablar bileşenini kullanarak tabları oluşturun.
+// Tabları, fonksiyona iletilen seçiciyle eşleşen DOM'daki öğeye ekleyin.
+//
+export { Tablar, tabEkleyici };
